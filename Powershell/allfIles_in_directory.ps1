85c6ceef7c5f051f61f75db5728f485c559a027d
astampedcarton <36453362+astampedcarton@users.noreply.github.com>
2025-05-05 21:28:43 +0200

    Initial commit

---
+++ Powershell/allfIles_in_directory.ps1
@@ -0,0 +1,7 @@
+﻿$DirPath = '\\quintiles.net\Enterprise\Organization\Biostatistics\Business Solutions\Solution Providers\General\GENIE\_ErrorReporCollation'
+$Outpath = '\\quintiles.net\Enterprise\Organization\Biostatistics\Business Solutions\Solution Providers\General\GENIE\_ErrorReporCollation\Services.txt'
+
+Get-ChildItem -Path $DirPath -Recurse | Out-File -FilePath $Outpath
+
+#Get-ChildItem -Path '\\quintiles.net\Enterprise\Organization\Biostatistics\Business Solutions\Solution Providers\General\GENIE\_ErrorReporCollation' -Recurse | Out-File -FilePath '\\quintiles.net\Enterprise\Organization\Biostatistics\Business Solutions\Solution Providers\General\GENIE\_ErrorReporCollation\Services.txt'
+
